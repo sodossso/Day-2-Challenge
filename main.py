8a7a0dee1cbbb0453767fa3af8da74d9d8eca965
Sofia <sofianunes96@gmail.com>
2024-07-23 16:27:40 +0000

    Made the code easier

--- main.py
+++ main.py
@@ -7,9 +7,7 @@
 print("Welcome to the tip calculator!")
 #added a space before the final " in order for the cursor to come a space away from the prompt
 bill=input("What was the total bill? $")
-
 tip_perc=input("How much tip would you like to give? 10, 12, or 15? ")
-
 numb_people=input("How many people to split the bill? ")
 #bill will be inputed with a "$" as a string and so we need to convert this to a float and in order to do so we need to remove the "$". I can do that by subscripting the characters that start in position 1 -->[1:]
 bill_per_person=round(float(bill)*(1+int(tip_perc)/100)/int(numb_people),2)
